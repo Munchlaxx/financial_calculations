57c8e198edcc52999735885f91cfbeb30f022f3b
John Mirschel <evan.mirschel@gmail.com>
2020-10-24 17:52:16 -0700

    Adding peer functionality

--- stock_calculator.py
+++ stock_calculator.py
@@ -1,1 +1,21 @@
 import json
+import requests
+import os
+
+
+class StockCalculator:
+    def __init__(self, stock):
+        self.stock = stock
+        self.finviz = "https://finnhub.io/api/v1"
+        self.api_key = os.environ['API_KEY']
+
+    def find_competition(self):
+        response = requests.get("{}/stock/peers?symbol={}&token={}".format(self.finviz, self.stock, self.api_key))
+        print(response.json())
+
+test = StockCalculator('AAPL')
+test.find_competition()
+
+
+
+
